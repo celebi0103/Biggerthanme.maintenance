53def053985d76b08ac856e9a3af4ae936b23c01
ACELEBI <ali.celebi@ynov.com>
2023-03-15 17:36:20 +0100

    last push before hosting

--- main.go
+++ main.go
@@ -12,23 +12,7 @@ func main() {
 }
 func Handlefunc() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		template := template.Must(template.ParseFiles("Page/accueil.html", "templates/header.html", "templates/footer.html"))
-		if r.Method != http.MethodPost {
-			template.Execute(w, "")
-			return
-		}
-	})
-
-	http.HandleFunc("/maintenance", func(w http.ResponseWriter, r *http.Request) {
-		template := template.Must(template.ParseFiles("Page/index.html", "templates/header.html", "templates/footer.html"))
-		if r.Method != http.MethodPost {
-			template.Execute(w, "")
-			return
-		}
-	})
-
-	http.HandleFunc("/services", func(w http.ResponseWriter, r *http.Request) {
-		template := template.Must(template.ParseFiles("Page/nos_services.html", "templates/header.html", "templates/footer.html"))
+		template := template.Must(template.ParseFiles("./index.html"))
 		if r.Method != http.MethodPost {
 			template.Execute(w, "")
 			return
