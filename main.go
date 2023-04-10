652d81b6a71fad9087e92755bb3a0202a232e407
celebi0103 <103508135+celebi0103@users.noreply.github.com>
2023-04-10 12:05:47 +0200

    Update main

--- main.go
+++ main.go
@@ -90,7 +90,7 @@ func Handlefunc() {
 	})
 
 	fs := http.FileServer(http.Dir("Static/"))
-	http.Handle("/Static/", http.StripPrefix("/Static/", fs))
-	fmt.Println("http://biggerthanme.fr:8080")
-	http.ListenAndServe(":8080", nil)
+http.Handle("/Static/", http.StripPrefix("/Static/", fs))
+fmt.Println("http://biggerthanme.fr:8080")
+http.ListenAndServe(":8080", nil)
 }
