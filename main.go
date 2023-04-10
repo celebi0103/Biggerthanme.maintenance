969be17985b53c315a1c1345201c042be2996581
celebi0103 <103508135+celebi0103@users.noreply.github.com>
2023-04-10 12:26:07 +0200

    v1

--- main.go
+++ main.go
@@ -7,113 +7,90 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
-	http.HandleFunc("/equipes", equipesHandler)
-	http.HandleFunc("/blog", blogHandler)
-	http.HandleFunc("/articles", articlesHandler)
-	http.HandleFunc("/articlesB", articlesBHandler)
-	http.HandleFunc("/articlesBasket", articlesBasketHandler)
-	http.HandleFunc("/articlesHandi", articlesHandiHandler)
-	http.HandleFunc("/services", servicesHandler)
-	http.HandleFunc("/articlesdanse", articlesDanseHandler)
-	http.HandleFunc("/articlesfede", articlesFedeHandler)
+
+	Handlefunc()
+}
+func Handlefunc() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/index.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+
+	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/contact.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+	http.HandleFunc("/equipes", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/equipes.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+	http.HandleFunc("/blog", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/blog.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+
+	http.HandleFunc("/articles", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/articles.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+
+	http.HandleFunc("/articlesB", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/articleBoxe.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+
+	http.HandleFunc("/articlesBasket", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/articlebasket.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+
+	http.HandleFunc("/articlesHandi", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/articlehandi.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+
+	http.HandleFunc("/services", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/nos_services.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+
+	http.HandleFunc("/articlesdanse", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("./Page/articleDanse.html", "templates/header.html", "templates/footer.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
 
 	fs := http.FileServer(http.Dir("Static/"))
 	http.Handle("/Static/", http.StripPrefix("/Static/", fs))
-
-	fmt.Println("Listening on port 80...")
-	err := http.ListenAndServe(":80", nil)
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("https://biggerthanme:8080")
+	http.ListenAndServe("https://biggerthanme:8080", nil)
 }
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	template := template.Must(template.ParseFiles("./Page/index.html", "templates/header.html", "templates/footer.html"))
-	if r.Method != http.MethodPost {
-		template.Execute(w, nil)
-		return
-	}
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	template := template.Must(template.ParseFiles("./Page/contact.html", "templates/header.html", "templates/footer.html"))
-	if r.Method != http.MethodPost {
-		template.Execute(w, nil)
-		return
-	}
-}
-
-func equipesHandler(w http.ResponseWriter, r *http.Request) {
-	template := template.Must(template.ParseFiles("./Page/equipes.html", "templates/header.html", "templates/footer.html"))
-	if r.Method != http.MethodPost {
-		template.Execute(w, nil)
-		return
-	}
-}
-
-func blogHandler(w http.ResponseWriter, r *http.Request) {
-	template := template.Must(template.ParseFiles("./Page/blog.html", "templates/header.html", "templates/footer.html"))
-	if r.Method != http.MethodPost {
-		template.Execute(w, nil)
-		return
-	}
-}
-
-func articlesHandler(w http.ResponseWriter, r *http.Request) {
-	template := template.Must(template.ParseFiles("./Page/articles.html", "templates/header.html", "templates/footer.html"))
-	if r.Method != http.MethodPost {
-		template.Execute(w, nil)
-		return
-	}
-}
-
-func articlesBHandler(w http.ResponseWriter, r *http.Request) {
-	template := template.Must(template.ParseFiles("./Page/articleBoxe.html", "templates/header.html", "templates/footer.html"))
-	if r.Method != http.MethodPost {
-		template.Execute(w, nil)
-		return
-	}
-}
-
-func articlesBasketHandler(w http.ResponseWriter, r *http.Request) {
-	template := template.Must(template.ParseFiles("./Page/articlebasket.html", "templates/header.html", "templates/footer.html"))
-	if r.Method != http.MethodPost {
-		template.Execute(w, nil)
-		return
-	}
-}
-
-func articlesHandiHandler(w http.ResponseWriter, r *http.Request) {
-	template := template.Must(template.ParseFiles("./Page/articlehandi.html", "templates/header.html", "templates/footer.html"))
-	if r.Method != http.MethodPost {
-		template.Execute(w, nil)
-		return
-	}
-}
-
-func servicesHandler(w http.ResponseWriter, r *http.Request) {
-	template := template.Must(template.ParseFiles("./Page/nos_services.html", "templates/header.html", "templates/footer.html"))
-	if r.Method != http.MethodPost {
-		template.Execute(w, nil)
-		return
-	}
-}
-
-func articlesDanseHandler(w http.ResponseWriter, r *http.Request) {
-	template := template.Must(template.ParseFiles("./Page/articleDanse.html", "templates/header.html", "templates/footer.html"))
-	if r.Method != http.Method {
-			template.Execute(w, nil)
-		return
-	}
-}
-
-func articlesFedeHandler(w http.ResponseWriter, r *http.Request) {
-	template := template.Must(template.ParseFiles("./Page/articlefederation.html", "templates/header.html", "templates/footer.html"))
-	if r.Method != http.Method {
-			template.Execute(w, nil)
-		return
-	}
-}
-http.ListenAndServe(":80", http.HandlerFunc(Handlefunc))
