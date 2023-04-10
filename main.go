d77f4157f13b5ff4b78c961b97dd681013b0d302
celebi0103 <103508135+celebi0103@users.noreply.github.com>
2023-04-10 11:36:04 +0200

    updates1

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 }
 func Handlefunc() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		template := template.Must(template.ParseFiles("./Page/index.html", "templates/header.html", "templates/footer.html"))
+		template := template.Must(template.ParseFiles("../Page/index.html", "templates/header.html", "templates/footer.html"))
 		if r.Method != http.MethodPost {
 			template.Execute(w, "")
 			return
